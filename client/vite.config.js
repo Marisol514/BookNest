0d9215c1b03c4eba9fde1e01b74806292cbc9153
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-06-01 11:47:45 -0700

    updated the server.js file and the vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the base path is correct for production
   server: {
     port: 3000,
     open: true,
@@ -14,5 +14,8 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    outDir: 'dist',
   }
 })
--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4'); 
 const path = require('path');
-const { authMiddleware } = require('./utils/auth')
+const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -28,7 +27,7 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist/assets')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
@@ -45,3 +44,4 @@ const startApolloServer = async () => {
 
 // Call the async function to start the server
 startApolloServer();
+
